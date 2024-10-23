2149006c46cd3bbd52335db300c13adc15545fef
Samir Jose Giil <144859085+SamirJoseGil@users.noreply.github.com>
2024-10-22 20:59:39 -0500

    update title

--- src/components/Bienvenida.tsx
+++ src/components/Bienvenida.tsx
@@ -5,8 +5,7 @@ function Bienvenida() {
     return (
         <div className='custom-body'>
             <div className="welcome-container">
-                <h1>Manual Digital de Ensamble y Desensamble</h1>
-                <h2>TechEduca</h2>
+                <h1 className="custom-title-welcome">TechEduca</h1>
                 <p>Hecho por Maryoni Garcia Vargas y Salome Serna Sepulveda</p>
                 <p>Hecho con HTML, CSS y JavaScript</p>
                 <button className="btn-primary"><a href="/Manual">Comenzar</a></button>
